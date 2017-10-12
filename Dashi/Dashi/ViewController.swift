18c9d9b069538a52d7b1e6408ec34f1c2358daac
Eric Smith <eric@smu.edu>
2017-10-12 14:24:04 -0500

    App is running

---
+++ Dashi/Dashi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dashi
+//
+//  Created by Eric Smith on 10/12/17.
+//  Copyright © 2017 Eric Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
