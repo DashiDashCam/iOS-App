0b66ea6cfba1a2f0608bfe1d348b8d6b913504e8
arslan0060 <amemon@smu.edu>
2017-10-14 17:39:48 -0500

    can take vids

--- Dashi/Dashi/ViewController.swift
+++ Dashi/Dashi/ViewController.swift
@@ -1,162 +1,222 @@
-//
-//  ViewController.swift
-//  Camera
-//
-//  Created by Rizwan on 16/06/17.
-//  Copyright © 2017 Rizwan. All rights reserved.
-//
-
-import UIKit
-import AVFoundation
-
-class ViewController: UIViewController {
-
-    @IBOutlet weak var previewView: UIView!
-    @IBOutlet weak var captureButton: UIButton!
-    @IBOutlet weak var messageLabel: UILabel!
+    import UIKit
+    import AVFoundation
     
-    var captureSession: AVCaptureSession?
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    var capturePhotoOutput: AVCapturePhotoOutput?
-    var qrCodeFrameView: UIView?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
         
-        captureButton.layer.cornerRadius = captureButton.frame.size.width / 2
-        captureButton.clipsToBounds = true
         
-        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video as the media type parameter
-        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
         
-        do {
-            // Get an instance of the AVCaptureDeviceInput class using the previous deivce object
-            let input = try AVCaptureDeviceInput(device: captureDevice)
+        @IBOutlet var camPreview: UIView!
+        
+        let cameraButton = UIView()
+        
+        let captureSession = AVCaptureSession()
+        
+        let movieOutput = AVCaptureMovieFileOutput()
+        
+        var previewLayer: AVCaptureVideoPreviewLayer!
+        
+        var activeInput: AVCaptureDeviceInput!
+        
+        var outputURL: URL!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
             
-            // Initialize the captureSession object
-            captureSession = AVCaptureSession()
-            
-            // Set the input devcie on the capture session
-            captureSession?.addInput(input)
-            
-            // Get an instance of ACCapturePhotoOutput class
-            capturePhotoOutput = AVCapturePhotoOutput()
-            capturePhotoOutput?.isHighResolutionCaptureEnabled = true
-            
-            // Set the output on the capture session
-            captureSession?.addOutput(capturePhotoOutput)
-            
-            // Initialize a AVCaptureMetadataOutput object and set it as the input device
-            let captureMetadataOutput = AVCaptureMetadataOutput()
-            captureSession?.addOutput(captureMetadataOutput)
-
-            // Set delegate and use the default dispatch queue to execute the call back
-            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
-            
-            //Initialise the video preview layer and add it as a sublayer to the viewPreview view's layer
-            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
-            videoPreviewLayer?.frame = view.layer.bounds
-            previewView.layer.addSublayer(videoPreviewLayer!)
-            
-            //start video capture
-            captureSession?.startRunning()
-            
-            messageLabel.isHidden = true
-            
-            //Initialize QR Code Frame to highlight the QR code
-            qrCodeFrameView = UIView()
-            
-            if let qrCodeFrameView = qrCodeFrameView {
-                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
-                qrCodeFrameView.layer.borderWidth = 2
-                view.addSubview(qrCodeFrameView)
-                view.bringSubview(toFront: qrCodeFrameView)
+            if setupSession() {
+                setupPreview()
+                startSession()
             }
-        } catch {
-            //If any error occurs, simply print it out
-            print(error)
-            return
-        }
-    
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func onTapTakePhoto(_ sender: Any) {
-        // Make sure capturePhotoOutput is valid
-        guard let capturePhotoOutput = self.capturePhotoOutput else { return }
-        
-        // Get an instance of AVCapturePhotoSettings class
-        let photoSettings = AVCapturePhotoSettings()
-        
-        // Set photo settings for our need
-        photoSettings.isAutoStillImageStabilizationEnabled = true
-        photoSettings.isHighResolutionPhotoEnabled = true
-        photoSettings.flashMode = .off
-        
-        // Call capturePhoto method by passing our photo settings and a delegate implementing AVCapturePhotoCaptureDelegate
-        capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
-    }
-}
-
-extension ViewController : AVCapturePhotoCaptureDelegate {
-    func capture(_ captureOutput: AVCapturePhotoOutput,
-                 didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?,
-                 previewPhotoSampleBuffer: CMSampleBuffer?,
-                 resolvedSettings: AVCaptureResolvedPhotoSettings,
-                 bracketSettings: AVCaptureBracketedStillImageSettings?,
-                 error: Error?) {
-        // Make sure we get some photo sample buffer
-        guard error == nil,
-            let photoSampleBuffer = photoSampleBuffer else {
-            print("Error capturing photo: \(String(describing: error))")
-            return
-        }
-        
-        // Convert photo same buffer to a jpeg image data by using AVCapturePhotoOutput
-        guard let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else {
-            return
-        }
-        
-        // Initialise an UIImage with our image data
-        let capturedImage = UIImage.init(data: imageData , scale: 1.0)
-        if let image = capturedImage {
-            // Save our captured image to photos album
-            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-        }
-    }
-}
-
-extension ViewController : AVCaptureMetadataOutputObjectsDelegate {
-    func captureOutput(_ captureOutput: AVCaptureOutput!,
-                       didOutputMetadataObjects metadataObjects: [Any]!,
-                       from connection: AVCaptureConnection!) {
-        // Check if the metadataObjects array is not nil and it contains at least one object.
-        if metadataObjects == nil || metadataObjects.count == 0 {
-            qrCodeFrameView?.frame = CGRect.zero
-            messageLabel.isHidden = true
-            return
-        }
-        
-        // Get the metadata object.
-        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
-        
-        if metadataObj.type == AVMetadataObjectTypeQRCode {
-            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
-            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
-            qrCodeFrameView?.frame = barCodeObject!.bounds
             
-            if metadataObj.stringValue != nil {
-                messageLabel.isHidden = false
-                messageLabel.text = metadataObj.stringValue
-                debugPrint(metadataObj.stringValue)
+            cameraButton.isUserInteractionEnabled = true
+            
+            let cameraButtonRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.startCapture))
+            
+            cameraButton.addGestureRecognizer(cameraButtonRecognizer)
+            
+            cameraButton.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+            
+            cameraButton.backgroundColor = UIColor.red
+            
+            camPreview.addSubview(cameraButton)
+            
+        }
+        
+        func setupPreview() {
+            // Configure previewLayer
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer.frame = camPreview.bounds
+            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            camPreview.layer.addSublayer(previewLayer)
+        }
+        
+        //MARK:- Setup Camera
+        
+        func setupSession() -> Bool {
+            
+            captureSession.sessionPreset = AVCaptureSessionPresetHigh
+            
+            // Setup Camera
+            let camera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+            
+            do {
+                let input = try AVCaptureDeviceInput(device: camera)
+                if captureSession.canAddInput(input) {
+                    captureSession.addInput(input)
+                    activeInput = input
+                }
+            } catch {
+                print("Error setting device video input: \(error)")
+                return false
+            }
+            
+            // Setup Microphone
+            let microphone = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio)
+            
+            do {
+                let micInput = try AVCaptureDeviceInput(device: microphone)
+                if captureSession.canAddInput(micInput) {
+                    captureSession.addInput(micInput)
+                }
+            } catch {
+                print("Error setting device audio input: \(error)")
+                return false
+            }
+            
+            
+            // Movie output
+            if captureSession.canAddOutput(movieOutput) {
+                captureSession.addOutput(movieOutput)
+            }
+            
+            return true
+        }
+        
+        func setupCaptureMode(_ mode: Int) {
+            // Video Mode
+            
+        }
+        
+        //MARK:- Camera Session
+        func startSession() {
+            
+            
+            if !captureSession.isRunning {
+                videoQueue().async {
+                    self.captureSession.startRunning()
+                }
             }
         }
+        
+        func stopSession() {
+            if captureSession.isRunning {
+                videoQueue().async {
+                    self.captureSession.stopRunning()
+                }
+            }
+        }
+        
+        func videoQueue() -> DispatchQueue {
+            return DispatchQueue.main
+        }
+        
+        
+        
+        func currentVideoOrientation() -> AVCaptureVideoOrientation {
+            var orientation: AVCaptureVideoOrientation
+            
+            switch UIDevice.current.orientation {
+            case .portrait:
+                orientation = AVCaptureVideoOrientation.portrait
+            case .landscapeRight:
+                orientation = AVCaptureVideoOrientation.landscapeLeft
+            case .portraitUpsideDown:
+                orientation = AVCaptureVideoOrientation.portraitUpsideDown
+            default:
+                orientation = AVCaptureVideoOrientation.landscapeRight
+            }
+            
+            return orientation
+        }
+        
+        func startCapture() {
+            
+            startRecording()
+            
+        }
+        
+        //EDIT 1: I FORGOT THIS AT FIRST
+        
+        func tempURL() -> URL? {
+            let directory = NSTemporaryDirectory() as NSString
+            
+            if directory != "" {
+                let path = directory.appendingPathComponent(NSUUID().uuidString + ".mp4")
+                return URL(fileURLWithPath: path)
+            }
+            
+            return nil
+        }
+        
+        
+        func startRecording() {
+            
+            if movieOutput.isRecording == false {
+                
+                let connection = movieOutput.connection(withMediaType: AVMediaTypeVideo)
+                if (connection?.isVideoOrientationSupported)! {
+                    connection?.videoOrientation = currentVideoOrientation()
+                }
+                
+                if (connection?.isVideoStabilizationSupported)! {
+                    connection?.preferredVideoStabilizationMode = AVCaptureVideoStabilizationMode.auto
+                }
+                
+                let device = activeInput.device
+                if (device?.isSmoothAutoFocusSupported)! {
+                    do {
+                        try device?.lockForConfiguration()
+                        device?.isSmoothAutoFocusEnabled = false
+                        device?.unlockForConfiguration()
+                    } catch {
+                        print("Error setting configuration: \(error)")
+                    }
+                    
+                }
+                
+                //EDIT2: And I forgot this
+                outputURL = tempURL()
+                movieOutput.startRecording(toOutputFileURL: outputURL, recordingDelegate: self)
+                
+            }
+            else {
+                stopRecording()
+            }
+            
+        }
+        
+        func stopRecording() {
+            
+            if movieOutput.isRecording == true {
+                movieOutput.stopRecording()
+            }
+        }
+        
+        func capture(_ captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAt fileURL: URL!, fromConnections connections: [Any]!) {
+            UISaveVideoAtPathToSavedPhotosAlbum(fileURL.relativePath, self, nil, nil)
+            
+        }
+        
+        func capture(_ captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAt outputFileURL: URL!, fromConnections connections: [Any]!, error: Error!) {
+            if (error != nil) {
+                print("Error recording movie: \(error!.localizedDescription)")
+            } else {
+                
+                _ = outputURL as URL
+                
+            }
+            outputURL = nil
+        }
+        
+        
+        
     }
-}
-
