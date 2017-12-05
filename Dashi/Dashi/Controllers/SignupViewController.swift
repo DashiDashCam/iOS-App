a36aa72c24de92600893aa924b796737ebbf3bb6
Eric Smith <eric@smu.edu>
2017-12-05 14:59:03 -0600

    styled sign up button on landscape

--- Dashi/Dashi/Controllers/SignupViewController.swift
+++ Dashi/Dashi/Controllers/SignupViewController.swift
@@ -12,7 +12,7 @@ class SignupViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateConstraints()
         // Do any additional setup after loading the view.
     }
 
@@ -20,20 +20,45 @@ class SignupViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    @IBAction func signUpPushed(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+    @IBAction func signUpPushed(_: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    
+
+    override func willTransition(to _: UITraitCollection, with _: UIViewControllerTransitionCoordinator) {
+
+        updateConstraints()
+    }
+
+    @IBOutlet weak var signUpButton: UIButton!
+
+    // updates the hardcoded contraints associated with this view
+    func updateConstraints() {
+        // loop through view constraints
+        for constraint in view.constraints {
+            // the device is in landscape
+            if UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight {
+                // "Username" label above input
+                if constraint.identifier == "signUpMarginTop" {
+                    constraint.constant = 0
+                    signUpButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+                }
+            } else {
+                // "Username" label above input
+                if constraint.identifier == "signUpMarginTop" {
+                    constraint.constant = 20
+                }
+            }
+        }
+    }
+
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
