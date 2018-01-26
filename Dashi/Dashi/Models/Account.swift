8421de0d919f60df5bbf0f2de1f51faf6a6280c9
cahenk <cahenk95@gmail.com>
2018-01-25 20:33:23 -0600

    Implemented several routes, updated documentation comments

---
+++ Dashi/Dashi/Models/Account.swift
@@ -0,0 +1,46 @@
+//
+//  Account.swift
+//  Dashi
+//
+//  Created by Chris Henk on 1/25/18.
+//  Copyright © 2018 Senior Design. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class Account {
+    
+    // Protected members
+    var created: Date
+    var id: Int
+    
+    // Public members
+    public var fullName: String
+    public var email: String
+    
+    init(account: JSON) {
+        self.fullName = account["fullName"].stringValue
+        self.created = Date().addingTimeInterval(account["created"].doubleValue)
+        self.id = account["id"].intValue
+        self.email = account["email"].stringValue
+    }
+    
+    public func toParameters() -> Parameters {
+        let parameters: Parameters = [
+            "id": self.id,
+            "fullName": self.fullName,
+            "email": self.email
+        ]
+        return parameters
+    }
+    
+    public func getCreated() -> Date {
+        return self.created
+    }
+    
+    public func getId() -> Int {
+        return self.id
+    }
+}
---
+++ Dashi/Dashi/Models/Video.swift
@@ -0,0 +1,60 @@
+//
+//  Video.swift
+//  Dashi
+//
+//  Created by Chris Henk on 1/25/18.
+//  Copyright © 2018 Senior Design. All rights reserved.
+//
+
+import Foundation
+import AVKit
+import SwiftyJSON
+
+class Video {
+    
+    // Protected members
+    var content: AVURLAsset?
+    var started: Date
+    var length: Int
+    var size: Int
+    var id: Int
+    
+    init(id: Int, started: Date, length: Int, size: Int, content: AVURLAsset? = nil) {
+        self.id = id
+        self.started = started
+        self.length = length
+        self.size = size
+        self.content = content!
+    }
+    
+    init(video: JSON) {
+        self.id = video["id"].intValue
+        self.started = Date().addingTimeInterval(video["started"].doubleValue)
+        self.length = video["length"].intValue
+        self.size = video["size"].intValue
+    }
+    
+    public func setContent(content: AVURLAsset) {
+        self.content = content
+    }
+    
+    public func getContent() -> AVURLAsset {
+        return self.content!
+    }
+    
+    public func getStarted() -> Date {
+        return self.started
+    }
+    
+    public func getLength() -> Int {
+        return self.length
+    }
+    
+    public func getSize() -> Int {
+        return self.size
+    }
+    
+    public func getId() -> Int {
+        return self.id
+    }
+}
