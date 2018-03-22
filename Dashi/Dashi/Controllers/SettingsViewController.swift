fb7a41ee562f0182abb9a1248a11abf3c0bfaabe
Michael Gilbert <michaelgilbert@Michaels-Mac.local>
2018-03-22 14:31:56 -0700

    BACK BUTTON AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Dashi/Dashi/Controllers/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  Dashi
+//
+//  Created by Michael Gilbert on 3/22/18.
+//  Copyright © 2018 Senior Design. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import CoreMedia
+import CoreLocation
+
+class SettingsViewController: UIViewController {
+    let appDelegate =
+        UIApplication.shared.delegate as? AppDelegate
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create tap gesture recognizer for when user taps thumbnail
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(VideoDetailViewController.imageTapped(gesture:)))
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender _: Any?) {
+        //         Get the new view controller using segue.destinationViewController.
+        //         Pass the selected object to the new view controller.
+        let preview = segue.destination as! SettingsViewController
+    }
+    
+}
