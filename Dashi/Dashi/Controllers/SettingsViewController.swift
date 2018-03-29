de2d10de61af2ec8f9945c55a6e063109d83f3e5
Michael Gilbert <michaelgilbert@Michaels-Mac.local>
2018-03-29 14:14:31 -0700

    fixed the f*cking ui pickers

--- Dashi/Dashi/Controllers/SettingsViewController.swift
+++ Dashi/Dashi/Controllers/SettingsViewController.swift
@@ -11,29 +11,68 @@ import AVFoundation
 import CoreMedia
 import CoreLocation
 
-class SettingsViewController: UIViewController{
+class SettingsViewController: UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
-    let appDelegate =
-        UIApplication.shared.delegate as? AppDelegate
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if(pickerView == retentionLocal){
+            return retentionLocalSource.count
+        }
+        else if(pickerView == storageLocal){
+            return storageLocalSource.count
+        }
+        else if(pickerView == retentionCloud){
+            return retentionCloudSource.count
+        }
+        else{
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if(pickerView == retentionLocal){
+            return retentionLocalSource[row]
+        }
+        else if(pickerView == storageLocal){
+            return storageLocalSource[row]
+        }
+        else if(pickerView == retentionCloud){
+            return retentionCloudSource[row]
+        }
+        else{
+            return ""
+        }
+    }
+    
+    @IBOutlet weak var retentionLocal: UIPickerView!
+    @IBOutlet weak var storageLocal: UIPickerView!
+    @IBOutlet weak var retentionCloud: UIPickerView!
+    
+    var retentionLocalSource: [String] = [String]()
+    var storageLocalSource: [String] = [String]()
+    var retentionCloudSource: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.hidesBackButton = false;
         
-        //connect pickers to data
-        //I think this works by associating the picker name
-        //with the array (pickerName)Data automatically
+        //connect pickers to view
+        retentionLocal.delegate = self
+        retentionLocal.dataSource = self
+        retentionCloud.delegate = self
+        retentionCloud.dataSource = self
+        storageLocal.delegate = self
+        storageLocal.dataSource = self
         
-        
-        // create tap gesture recognizer for when user taps thumbnail
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(VideoDetailViewController.imageTapped(gesture:)))
+        retentionLocalSource = ["hi","bye"]
+        storageLocalSource = ["hi2","bye2"]
+        retentionCloudSource = ["hi3","my","bye3"]
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender _: Any?) {
-        //         Get the new view controller using segue.destinationViewController.
-        //         Pass the selected object to the new view controller.
-        let preview = segue.destination as! SettingsViewController
     }
     
 }
